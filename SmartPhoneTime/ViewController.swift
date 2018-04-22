fa31f02264ace1e95129e52f861dd1a4a08e5826
牧野真之 <saneyuki@makinomasayukinomacbook-ea.local>
2018-04-22 14:27:39 +0900

    Initial Commit

---
+++ SmartPhoneTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartPhoneTime
+//
+//  Created by 牧野真之 on 2018/04/22.
+//  Copyright © 2018年 Lame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
